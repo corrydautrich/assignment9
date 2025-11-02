99bb329127cb8ecdd1e78e0aa2a4ec725817f223
corrydautrich <corry.dautrich@icloud.com>
2025-11-02 13:09:53 -0600

    got some gifs to show up on the screen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 console.log("script.js loaded");
+
+
+const api_key = "Cfc6Jld9ZaYQhpxgdaMfI97gkeW29ahu"
+
+const searchInput = document.querySelector("#search-input");
+const gifContainer = document.querySelector("#gif-container");
+
+async function getGifs(searchTerm = "cats") {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=Cfc6Jld9ZaYQhpxgdaMfI97gkeW29ahu&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    
+    const images = data.data.map(gif => gif.images.original.url);
+    console.log("Fetched GIFs for:", searchTerm, images);
+
+    gifContainer.innerHTML = "";
+
+    for (const url of images) {
+        const col = document.createElement("div");
+        col.className = "col-md-3 mb-3";
+
+        const img = document.createElement("img");
+        img.src = url;
+        img.className = "img-fluid rounded shadow";
+
+        col.appendChild(img);
+        gifContainer.appendChild(col);
+    }
+}
+
+getGifs();
+
+searchInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        const term = searchInput.ariaValueMax.trim();
+        if (term !== "") {
+            getGifs(term);
+        }
+    }
+})
+
